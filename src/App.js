8c2b3aea1907b293ce0970a40a9e99a161ab021a
caelinp <cpbluewhale@gmail.com>
2023-08-01 16:02:45 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import background0 from "./img/icon.png"
 
-export default App;
+function App() {
+    return (
+        <div>
+            <nav class="navbar background">
+                <ul class="nav-list">
+                    <div class="logo">
+                        <img src=
+{background0} />
+                    </div>
+                    <li><a href="#our-products">Our Products</a></li>
+                    <li><a href="#about-us">About Us</a></li>
+                    <li><a href="#contact-us">Contact Us</a></li>
+                </ul>
+  
+            </nav>
+  
+           <section class="section">
+                <div class="box-main">
+                    <div class="firstHalf">
+                        <h1 class="text-big">
+                           Coffee 1
+                        </h1>
+                        <p class="text-small">
+                          Coffee 1
+                        </p>
+                    </div>
+                </div>
+            </section>
+            <section class="section">
+                <div class="box-main">
+                    <div class="secondHalf">
+                        <h1 class="text-big" id="program">
+                            Coffee 2
+                        </h1>
+                        <p class="text-small">
+                            Coffee 2
+                        </p>
+                    </div>
+                </div>
+            </section>
+            <section class="section">
+                <div class="box-main">
+                    <div class="secondHalf">
+                        <h1 class="text-big" id="program">
+                            Coffee 3
+                        </h1>
+                        <p class="text-small">
+                            Coffee 3
+                        </p>
+                    </div>
+                </div>
+            </section>
+            <section class="section">
+                <div class="box-main">
+                    <div class="secondHalf">
+                        <h1 class="text-big" id="program">
+                            Coffee 4
+                        </h1>
+                        <p class="text-small">
+                            Coffee 4
+                        </p>
+                    </div>
+                </div>
+            </section>
+            <footer className="footer">
+                <p className="text-footer">
+                    Copyright ©-All rights are reserved
+                </p>
+            </footer>
+        </div>
+    )
+}
+  
+export default App
