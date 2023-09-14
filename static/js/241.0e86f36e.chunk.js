e5bcf46a42bf217b6ff9d40bbddc5bb467251917
caelinp <cpbluewhale@gmail.com>
2023-09-14 15:48:20 -0700

    Updates

---
+++ static/js/241.0e86f36e.chunk.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkrider_coffee_roaster=self.webpackChunkrider_coffee_roaster||[]).push([[241],{241:function(e,r,s){e.exports=s.p+"static/media/bean.227a496b4c665f6bbd96.png"}}]);
