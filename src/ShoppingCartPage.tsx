59a5a75e8d837d42d2ef96d090344d382a2c6d83
caelinp <cpbluewhale@gmail.com>
2023-09-20 15:16:02 -0700

    shopping cart page more functional

--- src/ShoppingCartPage.tsx
+++ src/ShoppingCartPage.tsx
@@ -1,12 +1,13 @@
 import React, { useState, useEffect, useMemo } from 'react';
 import './ShoppingCartPage.css';
-
+import { Link } from 'react-router-dom';
 import { createSelector } from 'reselect';
 import { useDispatch, useSelector } from 'react-redux';
 import productsData from './json/products.json';
 import { updateCoffeeBagItem, removeCoffeeBagItem, clearCart, CartState } from './CartSlice';
 import CoffeeBagOrderItem from './OrderItem';
 import DynamicImage from './DynamicImage';
+import {formatString} from './AllProductsPage';
 
 interface Product {
   id: string;
@@ -111,14 +112,20 @@ const ShoppingCartPage = () => {
   };
 
   const handleQuantityChange = (e: React.ChangeEvent<HTMLInputElement>, orderItem: CoffeeBagOrderItem) => {
-    const quantityUpdate = e.target.value;
-    if (typeof quantityUpdate === 'number') {
-      if (Number.isInteger(quantityUpdate)) {
-        if (quantityUpdate > 0) {
-          orderItem.setOrderQuantity(parseInt(e.target.value));
-          dispatch(updateCoffeeBagItem(orderItem.toJSONString()));
-        }
-      }
+    const quantityUpdate = isNaN(parseInt(e.target.value)) ? 0 : parseInt(e.target.value);
+    orderItem.setOrderQuantity(quantityUpdate);
+    dispatch(updateCoffeeBagItem(orderItem.toJSONString()));
+  };
+
+  const handleQuantityBlur = (orderItem: CoffeeBagOrderItem) => {
+    if (orderItem.quantity === 0) {
+      dispatch(removeCoffeeBagItem(orderItem.id));
+    }
+  };
+
+  const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>, orderItem: CoffeeBagOrderItem) => {
+    if (event.key === 'Enter' && orderItem.quantity === 0) {
+      dispatch(removeCoffeeBagItem(orderItem.id));
     }
   };
 
@@ -126,80 +133,118 @@ const ShoppingCartPage = () => {
     dispatch(removeCoffeeBagItem(orderItem.id));
   };
 
+  const handleClearCart = () => {
+    dispatch(clearCart());
+  };
+
   const handleCheckout = () => {
     // Implement checkout logic here
   };
 
+   // Helper function to render order items or empty cart message
+   const renderOrderItems = () => {
+    if (cartItemsWithProductInfo.length === 0) {
+      // Render an empty cart message
+      return (
+        <div className="empty-cart-message">
+          <p>
+            Your cart is empty
+          </p>
+        </div>
+      );
+    } else {
+      // Render the list of order items
+      return cartItemsWithProductInfo.map((item) => (
+        <div className="order-item" key={item!.coffeeBagOrderItem!.id || ''}>
+          <Link
+            id="order-item-product-link"
+            to={`/rider-coffee-roaster/products/` + item!.product.id + "/" + formatString(item!.product.name) }
+            key={item!.product.id}
+            className="product-link"
+            >
+            <div className="item-option" id="order-item-product-name-and-image-container">
+              <label className="order-item-product-name">{item!.product.name || ''}</label><br></br>
+              <DynamicImage className="order-item-image" imageUrl={item!.product?.image} alt={item!.product.name} />
+            </div>
+          </Link>
+          <div className="item-option" id="weight-option-cart">
+            <label>Weight:</label>
+            <select className="option-input-field-cart" value={item!.coffeeBagOrderItem!.bagSize} onChange={(e) => handleWeightChange(e, item!.coffeeBagOrderItem)}>
+              {Object.keys(item!.product.pricing || {}).map((option) => (
+                <option key={option} value={option}>
+                  {option}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="item-option" id="grind-size-option-cart">
+            <label>Grind Size:</label>
+            <select className="option-input-field-cart" value={item!.coffeeBagOrderItem!.groundSize} onChange={(e) => handleGrindSizeChange(e, item!.coffeeBagOrderItem)}>
+              {grindSizeOptions.map((option) => (
+                <option key={option} value={option}>
+                  {option}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="item-option" id="quantity-option-cart">
+            <label>Quantity:</label>
+            <input
+              className="option-input-field-cart"
+              id="quantity-input-cart"
+              type="number"
+              value={item!.coffeeBagOrderItem!.quantity === 0 ? "0" : item!.coffeeBagOrderItem!.quantity.toString().replace(/^0+/, '')}
+              onChange={(e) => handleQuantityChange(e, item!.coffeeBagOrderItem)}
+              onBlur={() => handleQuantityBlur(item!.coffeeBagOrderItem)}
+              onKeyDown={(e) => handleKeyPress(e, item!.coffeeBagOrderItem)}
+            />
+          </div>
+          <div className="item-option" id="price-label-and-text-cart">
+            <label>Price:</label>
+            <p className="price-text">${parseFloat(item.price).toFixed(2)}</p>
+          </div>
+          <div className="item-option">
+            <button className="remove-button" onClick={() => handleRemoveItem(item!.coffeeBagOrderItem)}></button>
+          </div>
+        </div>
+      ));
+    }
+  };
+
   return (
     <div className="cart-page">
-      <div className="cart-content-and-bottom">
-        <div className="cart-content">
+    <div className="cart-content-and-bottom">
+      <div className="cart-content">
+        <div className="cart-summary">
           <div className="cart-header">
-            <h2>Your Cart</h2>
-            <button className="close-button" onClick={handleCloseCart}>
-              X
-            </button>
+            <h1>Your Cart</h1>
+            <button className="close-button" onClick={handleCloseCart}></button>
           </div>
-          <div className="order-items-container">
-            {cartItemsWithProductInfo.map((item) => (
-              <div className="order-item" key={item!.coffeeBagOrderItem!.id || ''}>
-                <div className="item-option" id="order-item-product-name-and-image-container">
-                  <label className="order-item-product-name">{item!.product.name || ''}</label><br></br>
-                  <DynamicImage className="order-item-image" imageUrl={item!.product?.image} alt={item!.product.name} />
-                </div>
-                <div className="item-option" id="weight-option-cart">
-                  <label>Weight:</label><br></br>
-                  <select className="option-input-field-cart" value={item!.coffeeBagOrderItem!.bagSize} onChange={(e) => handleWeightChange(e, item!.coffeeBagOrderItem)}>
-                    {Object.keys(item!.product.pricing || {}).map((option) => (
-                      <option key={option} value={option}>
-                        {option}
-                      </option>
-                    ))}
-                  </select>
-                </div>
-                <div className="item-option" id="grind-size-option-cart">
-                  <label>Grind Size:</label><br></br>
-                  <select className="option-input-field-cart" value={item!.coffeeBagOrderItem!.groundSize} onChange={(e) => handleGrindSizeChange(e, item!.coffeeBagOrderItem)}>
-                    {grindSizeOptions.map((option) => (
-                      <option key={option} value={option}>
-                        {option}
-                      </option>
-                    ))}
-                  </select>
-                </div>
-                <div className="item-option" id="quantity-option-cart">
-                  <label>Quantity:</label><br></br>
-                  <input
-                    className="option-input-field-cart"
-                    id="quantity-input-cart"
-                    type="number"
-                    value={item!.coffeeBagOrderItem!.quantity === 0 ? "0" : item!.coffeeBagOrderItem!.quantity.toString().replace(/^0+/, '')}
-                    onChange={(e) => handleQuantityChange(e, item!.coffeeBagOrderItem)}
-                  />
-                </div>
-                <div className="item-option" id="price-label-and-text-cart">
-                  <label>Price:</label>
-                  <p className="price-text">${parseFloat(item.price).toFixed(2)}</p>
-                </div>
-                <div className="item-option">
-                  <button className="remove-button" onClick={() => handleRemoveItem(item!.coffeeBagOrderItem)}>X</button>
-                </div>
-              </div>
-            ))}
-          </div>
-          <div className="cart-summary">
-            <div className="cart-total">
-              <label>Total:</label>
-              <p>${totalPrice.toFixed(2)}</p>
-            </div>
-            <button className="checkout-button" onClick={handleCheckout}>
-              Checkout
+          <div className="cart-item-count-and-clear">
+            <p className="cart-item-count">
+              {cartItemsWithProductInfo.length + (cartItemsWithProductInfo.length === 1 ? " Item in Cart" : " Items in Cart")} 
+            </p>
+            <button className="clear-cart-button" onClick={handleClearCart}>
+              Clear Cart
             </button>
           </div>
         </div>
+        <div className="order-items-container">
+          {renderOrderItems()}
+        </div>
+        <div className="cart-summary">
+          <div className="cart-total">
+            <label>{"Total: "}</label>
+            <p>${totalPrice.toFixed(2)}</p>
+          </div>
+          <button className="checkout-button" onClick={handleCheckout}>
+            Checkout
+          </button>
+        </div>
       </div>
     </div>
-  );
+  </div>
+);
 };
 
 export default ShoppingCartPage;
