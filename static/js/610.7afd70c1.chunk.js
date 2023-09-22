2f418641346c53a74e3cfbd60c4b09cda82c21cb
caelinp <cpbluewhale@gmail.com>
2023-09-22 16:46:57 -0700

    Updates

---
+++ static/js/610.7afd70c1.chunk.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkrider_coffee_roaster=self.webpackChunkrider_coffee_roaster||[]).push([[610],{9610:function(e,r,a){e.exports=a.p+"static/media/metal-logo.73a0906bce4b6aef0542.png"}}]);
