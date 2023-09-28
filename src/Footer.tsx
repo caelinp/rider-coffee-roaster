4c41ace39451b2ba0761d2370b71780d40943f13
caelinp <cpbluewhale@gmail.com>
2023-09-28 16:27:47 -0700

    reformatted footer

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -4,30 +4,29 @@ import './Footer.css';
 import { Link } from 'react-router-dom';
 import logo from './img/metal-logo.gif';
 
-
 const Footer: React.FC = () => {
   return (
     <div className="black-bar-bottom">
-        <div className="footer">
-            <div className="footer-links">
-                <a href="/">Home</a>
-                <a href="/featured-products/">Featured</a>
-                <a href="/products/">Products</a>
-                <a href="/about-us/">About Us</a>
-                <a href="/contact-us/">Contact Us</a>
-                <a href="/shopping-cart/">Your Cart</a>
-            </div>
-            <div className="copyright-and-logo">
-                <Link to="/">
-                    <img src={logo} alt="Rider Coffee Roaster Logo" className="logo-footer" />
-                </Link>
-                <div className="copyright">
-                    © 2023 Rider Coffee Roaster.
-                </div>
-            </div>
+      <div className="footer">
+        <div className="footer-links">
+          <Link to="/" className="footer-link">Home</Link>
+          <Link to="/featured-products/" className="footer-link">Featured</Link>
+          <Link to="/products/" className="footer-link">Products</Link>
+          <Link to="/about-us/" className="footer-link">About Us</Link>
+          <Link to="/contact-us/" className="footer-link">Contact Us</Link>
+          <Link to="/shopping-cart/" className="footer-link">Your Cart</Link>
         </div>
+        <div className="copyright-and-logo">
+          <Link to="/" className="logo-footer-link">
+            <img src={logo} alt="Rider Coffee Roaster Logo" className="logo-footer" />
+          </Link>
+          <div className="copyright">
+            © 2023 Rider Coffee Roaster.
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
 
-export default Footer
+export default Footer;
