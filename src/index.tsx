00362091afceba87b9de1c346502a5e3ddf9192a
Caelin <prestoncaelin@gmail.com>
2024-01-17 14:07:57 -0800

    added meta tags

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { store } from './Store';
 import App from './App';
 import './index.css';
+import './img/icon-with-text_gold.png';
 
 const rootElement = document.getElementById('root');
 if (rootElement) {
